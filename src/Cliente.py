ff6790a837bdde950c9c4b4dfbcb025be15822e2
Noelia-Ruiz <ruiz.noeliaromina@gmail.com>
2023-06-02 14:43:49 -0300

    Se añade la función saludar personalizada segun género. Se incluyen validaciones

--- src/Cliente.py
+++ src/Cliente.py
@@ -19,49 +19,85 @@ import Masaje #se importa la clase Masaje
 class Cliente:
 
     def __init__(self, nombre, apellido, genero, masaje): #constructor
-        self.nombre = nombre
-        self.apellido = apellido
-        self.genero = genero
-        self.masaje = Masaje
+        self._nombre = input("\nIngresá tu nombre: ")
+        self._apellido = input("Ingresá tu apellido: ")
+        self._genero = None #se lo instancia en None para validarla después
+        self._masaje = Masaje
         
-    def set_nombre(self, nombre):
-        self.nombre = nombre
-
-    def get_nombre(self):
-        return self.nombre
+    @property
+    def nombre(self):
+        return self._nombre
     
-    def set_apellido(self, apellido):
-        self.apellido = apellido
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
 
-    def get_apellido(self):
-        return self.apellido
+    @property
+    def apellido(self):
+        return self._apellido
 
-    def set_genero(self, genero):
-        self.genero = genero
+    
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
 
-    def get_genero(self):
-        return self.genero
+    @property
+    def genero(self):
+        return self._genero
+    
+    @genero.setter
+    def genero(self, genero):
+        self._genero = genero
 
-    def set_masaje(self, masaje):
-        self.masaje = masaje
-
-    def get_masaje(self):
-        return self.masaje
 
     def clienteNuevo(self):
-        nombre = input("\nIngresá tu nombre: ")
-        apellido = input("Ingresá tu apellido: ")
-        genero = input("Ingresá tu género: ")
-    
-        print('\nDatos ingresados: \n----------------------------')
-        print("Nombre: ", nombre)
-        print("Apellido: ", apellido)
-        print("Género: ", genero)
+        #se muestran los datos
+        print('')
+        print('Datos ingresados'.center(28,'-'))
+        print("Nombre: ", cliente.nombre)
+        print("Apellido: ", cliente.apellido)
+        print("Género: ", cliente.genero)
         print("----------------------------")
+
+    def saludar(cliente):
+        print(f'\n-----------Hola, {cliente._nombre}-----------')
+
+        opcion_valida = False #bandera, flag
+
+        while not opcion_valida:
+            cliente._genero = input("Ingresá tu género:\n      <F> -> si sos mujer\n      <M> -> Si sos hombre\n      <O>--< Para otro género\nIngresá un caracter: ")
+
+            hombre = 'M'
+            mujer = 'F'
+            otro = 'O'
+
+            if (cliente._genero) == hombre:
+                print('\n***********************************************************')
+                print(f'¡Hola {cliente._nombre} Bienvenido a nuestro centro de belleza y spa!')
+                opcion_valida = True
+            elif (cliente._genero) == mujer:
+                print('\n***********************************************************')
+                print(f'¡Hola {cliente._nombre} Bienvenida a nuestro centro de belleza y spa!')
+                opcion_valida = True
+            elif cliente._genero == otro:
+                print('\n***********************************************************')
+                print(f'¡Hola {cliente._nombre} Bienvenide a nuestro centro de belleza y spa!')
+                opcion_valida = True
+            elif cliente._genero != hombre or cliente._genero != mujer or cliente._genero != otro:
+                #llama de nuevo a la función
+                print('Opción no valida intentalo de nuevo')
+                print(''.center(32,'-'))
+        print('**'.center(59,'*'))
+
+
+    """
+    CargarMasaje : deberá contar con un método propio que ejecute el método de la clase Masaje, masajes().
+    """
     
 
 # se instancia un objeto de la clase Cliente
 cliente = Cliente("", "", "", "")
 #se llama al método para que se instancien los atributos
-cliente.clienteNuevo()
+cliente.saludar() #primero se le dá la bienvenida
+cliente.clienteNuevo()#después se le muestra los datos
 
