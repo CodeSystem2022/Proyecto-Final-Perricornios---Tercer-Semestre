748c1e30a9388b56c8b723ab14765e36b2dad760
Noe3467 <estefitrinidad@gmail.com>
2023-06-08 02:22:43 -0300

    Se agrega validación con bandera, no se permite el ingreso hasta que los datos sean correctos

--- src/Admin.py
+++ src/Admin.py
@@ -1,7 +1,7 @@
 class Admin:
     def __init__(self, user, password):
-        self._user = user
-        self._password = password
+        self._user = None
+        self._password = None
 
     #Métodos getters y setters
     @property
@@ -19,20 +19,26 @@ class Admin:
     @password.setter
     def password(self, password):
         self._password = password
-        
+
 
     def validar_user(self):
         datos_correctos = False #bandera
-
+        self._user = input('Ingrese el nombre de usuario o correo: ')
+        
         while not datos_correctos:
-            self._user = input('Ingresá nombre de usuario o correo: ')
-            self._password = input('Ingresá la contraseña: ')
+            
+            if self._user == 'admin' :
+                self._password = input('Ingrese la contraseña: ')
+                if self._password == '12345':
+                    datos_correctos = True
+                    print('------------------------------------------\nBienvenid@ al sistema de gestión de turnos\n--Ingresaste como usuario administrador--\n------------------------------------------')
 
-            if self._user == 'admin' and self._password == '12345':
-                datos_correctos = True
-                print('------------------------------------------\nBienvenid@ al sistema de gestión de turnos\n--Ingresaste como usuario administrador--\n------------------------------------------')
+                else:
+                    print('------------------------------------\n--------Contraseña incorrecta-------\n--------Intentalo nuevamente--------\n------------------------------------')
+
             else:
-                print('------------------------------------\n--Usuario o contraseña incorrectos--\n--------Intentalo nuevamente--------\n------------------------------------')
+                print('------------------------------------\n---------Usuario incorrecto---------\n--------Intentalo nuevamente--------\n------------------------------------')
+                self._user = input('Ingrese el nombre de usuario: ')
 
-admin = Admin('admin', '12345')
-admin.validar_user()
+admin = Admin('', '') #se instancia un objeto vacío de la clase para validar
+admin.validar_user() #se llama al método para validar el usuario
