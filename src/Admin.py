e9f49552a60d5f5bc296aa67ee41c850b1ef49f2
Noe3467 <estefitrinidad@gmail.com>
2023-06-08 00:56:35 -0300

    Se implementa el bucle while para validar usuario

--- src/Admin.py
+++ src/Admin.py
@@ -1,13 +1,20 @@
 class Admin:
     def __init__(self, user, password):
-        self._user = input('Ingresá nombre de usuario o correo: ')
-        self._password = input('Ingresá la contraseña: ')
+        self._user = user
+        self._password = password
 
     def validar_user(self):
-        if self._user == 'admin' and self._password == '12345':
-            print('------------------------------------------\nBienvenid@ al sistema de gestión de turnos\n--Ingresaste como usuario administrador--\n------------------------------------------')
-        else:
-            print('------------------------------------\n--Usuario o contraseña incorrectos--\n--------Intentalo nuevamente--------\n------------------------------------')
+        datos_correctos = False #bandera
+
+        while not datos_correctos:
+            self._user = input('Ingresá nombre de usuario o correo: ')
+            self._password = input('Ingresá la contraseña: ')
+            
+            if self._user == 'admin' and self._password == '12345':
+                datos_correctos = True
+                print('------------------------------------------\nBienvenid@ al sistema de gestión de turnos\n--Ingresaste como usuario administrador--\n------------------------------------------')
+            else:
+                print('------------------------------------\n--Usuario o contraseña incorrectos--\n--------Intentalo nuevamente--------\n------------------------------------')
 
 admin = Admin('admin', '12345')
 admin.validar_user()
