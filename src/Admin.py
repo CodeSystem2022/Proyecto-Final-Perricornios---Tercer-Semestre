21cb0d9dfa0c6e3f5649e38ba08bf030f18afef8
Noe3467 <estefitrinidad@gmail.com>
2023-06-08 01:01:36 -0300

    Se crean métodos getters y setters para la clase Admin

--- src/Admin.py
+++ src/Admin.py
@@ -3,13 +3,31 @@ class Admin:
         self._user = user
         self._password = password
 
+    #Métodos getters y setters
+    @property
+    def user(self):
+        return self._user
+    
+    @user.setter
+    def user(self, user):
+        self._user = user
+
+    @property
+    def password(self):
+        return self._password
+    
+    @password.setter
+    def password(self, password):
+        self._password = password
+        
+
     def validar_user(self):
         datos_correctos = False #bandera
 
         while not datos_correctos:
             self._user = input('Ingresá nombre de usuario o correo: ')
             self._password = input('Ingresá la contraseña: ')
-            
+
             if self._user == 'admin' and self._password == '12345':
                 datos_correctos = True
                 print('------------------------------------------\nBienvenid@ al sistema de gestión de turnos\n--Ingresaste como usuario administrador--\n------------------------------------------')
