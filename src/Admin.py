10a8f9ad56b86977240286dccd97031ec4405dcc
Noelia-Ruiz <ruiz.noeliaromina@gmail.com>
2023-06-11 03:02:14 -0300

    Se añaden 3 intentos para colocar la contraseña correcta

--- src/Admin.py
+++ src/Admin.py
@@ -22,23 +22,34 @@ class Admin:
 
 
     def validar_user(self):
-        datos_correctos = False #bandera
-        self._user = input('Ingrese el nombre de usuario o correo: ')
+        intentos = 4
+        datos_correctos = False #bandera o flag
+        self._user = input('Ingresá el nombre de usuario o correo: ')
         
-        while not datos_correctos:
+        while not datos_correctos and intentos > 0:
             
             if self._user == 'admin' :
-                self._password = input('Ingrese la contraseña: ')
+                self._password = input('Ingresá la contraseña: ')
                 if self._password == '12345':
                     datos_correctos = True
                     print('------------------------------------------\nBienvenid@ al sistema de gestión de turnos\n--Ingresaste como usuario administrador--\n------------------------------------------')
 
                 else:
+                    intentos -= 1 #disminuye la cantidad a 3 intentos restantes
                     print('------------------------------------\n--------Contraseña incorrecta-------\n--------Intentalo nuevamente--------\n------------------------------------')
 
+                    if intentos == 0: #si es cero, lo saca del sistema y lo devuelve al menú
+                        print('Demasiados intentos..\n       Acceso denegado..\n              ..Saliendo del menú...')
+                        break
+
+                    if intentos == 1:
+                        print(f'Te queda {intentos} restante.')
+                    else:
+                        print(f'Te quedan {intentos} intentos restantes.')
+    
             else:
                 print('------------------------------------\n---------Usuario incorrecto---------\n--------Intentalo nuevamente--------\n------------------------------------')
                 self._user = input('Ingrese el nombre de usuario: ')
-
+            
 admin = Admin('', '') #se instancia un objeto vacío de la clase para validar
 admin.validar_user() #se llama al método para validar el usuario
