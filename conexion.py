d1433e788ba70949915e9d7476cf29d9ff2e16dd
Asterisko19 <103151514+asterisko19@users.noreply.github.com>
2023-06-17 20:46:20 -0300

    Add files via upload

--- conexion.py
+++ conexion.py
@@ -9,18 +9,18 @@ class Conexion:
     _DB_PORT = '5432'
     _HOST = 'localhost'
     _conexion = None
-    _curson = None
+    _cursor = None
 
 
     @classmethod
     def obtenerConexion(cls):
         if cls._conexion is None:
             try:
-                cls._conexion = bd.connect(hots=cls._HOST,
-                                          user=cls._USERNAME,
-                                          password=cls._PASSWORD,
-                                          port=cls._DB_PORT,
-                                          database=cls._DATABASE)
+                cls._conexion = bd.connect(host=cls._HOST,
+                                           user=cls._USERNAME,
+                                           password=cls._PASSWORD,
+                                           port=cls._DB_PORT,
+                                           database=cls._DATABASE)
                 log.debug(f'Conexion Exitosa: {cls._conexion}')
                 return cls._conexion
             except Exception as e:
@@ -29,18 +29,18 @@ class Conexion:
         else:
             return cls._conexion
             
-@classmethod
-def obtenerCursor(cls):
-    if cls._cursor is None:
-        try:
-            cls._cursor = cls.obtenerConexion().cursor()
-            log.debug(f'Se abrio correcatamente el cursor: {cls._cursor}')
+    @classmethod
+    def obtenerCursor(cls):
+        if cls._cursor is None:
+            try:
+                cls._cursor = cls.obtenerConexion().cursor()
+                log.debug(f'Se abrio correcatamente el cursor: {cls._cursor}')
+                return cls._cursor
+            except Exception as e:
+                log.error(f'Ocurrio un error: {e}')
+        else:
             return cls._cursor
-        except Exception as e:
-            log.error(f'Ocurrio un error: {e}')
-    else:
-        return cls._cursor
     
 if __name__ == '__main__':
-    Conexion.obetenerConexion()
+    Conexion.obtenerConexion()
     Conexion.obtenerCursor()
---
+++ logger_base.py
@@ -0,0 +1,15 @@
+import logging as log
+
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('log.log'),
+                    log.StreamHandler()
+                ])
+if __name__ == '__main__':
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel warning')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel, critical')
