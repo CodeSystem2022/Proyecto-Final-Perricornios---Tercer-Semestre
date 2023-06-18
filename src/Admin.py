d953555f8c7c8da8f147a36dc4c9938a5e3c7e63
ChuequitoMendez <112666288+ChuequitoMendez@users.noreply.github.com>
2023-06-18 18:29:49 -0300

    Update Admin.py

--- src/Admin.py
+++ src/Admin.py
@@ -24,49 +24,38 @@ class Admin:
     print('\nElegiste la opción ingresar al sistema modo Administrador..\n---------------------------------------------------------')
 
     def validar_user(self):
-        intentos_user = 4
-        intentos_password = 4
+        intentos_login = 4
         datos_correctos = False #bandera o flag
         self._user = input('Ingresá el nombre de usuario o correo: ')
-        
-        while not datos_correctos and intentos_user > 0:
-            
-            if self._user == 'admin' :
-                while intentos_password > 0:
-                    self._password = input('Ingresá la contraseña: ')
-                    if self._password == '12345':
-                        datos_correctos = True
-                        print('------------------------------------------\nBienvenid@ al sistema de gestión de turnos\n--Ingresaste como usuario Administrador--\n------------------------------------------')
-                        break #sale del bucle
-                        #Derivarlo a las diferentes opciones de menú
-                        
-                    else:
-                        print(f'------------------------------------\n--------Contraseña incorrecta-------\n               {self._password}\n------------------------------------')
-                        intentos_password -= 1 #disminuye la cantidad a 3 intentos restantes
-                        if intentos_password == 0: #si es cero, lo saca del sistema y lo devuelve al menú
-                            print('Agotaste los intentos..\n       Acceso denegado..\n                Saliendo del menú...')
-                            break
-                        if intentos_password == 1:
-                            print(f'Te queda {intentos_password} restante.')
-                        else:
-                            print('         Intentalo nuevamente           \n------------------------------------')
-                            print(f'Te quedan {intentos_password} intentos restantes.')
-                break #sale de la ejecución, derivar al menú
-
-            else:           
-                intentos_user -=1
-                
-                if intentos_user == 0: #si es cero, lo saca del sistema y lo devuelve al menú
-                    print('\n---------------------------------\nAgotaste todos los intentos..\n       Acceso denegado..\n              ..Saliendo del menú...')
-                    break
-                if intentos_user == 1:
-                    print(f'    Te queda {intentos_user} intento restante.')
-                else:
-                    print('------------------------------------------------------------')
-                    print('Nombre de usuario o correo incorrecto. Intentalo nuevamente')
-                    print(f'Te quedan {intentos_user} intentos restantes.')
+        self._password = input('Ingresá la contraseña: ')
+        while not datos_correctos and intentos_login > 0:
+            intentos_login -= 1
+            if self._user == 'admin' and self._password == '12345':
+                datos_correctos = True
+                self.menu_admin()#se presenta el menu de opciones de administrador
+            elif intentos_login < 4 and intentos_login > 0:
+                print('Nombre de usuario o contraseña incorrecta. Intentalo nuevamente')
+                print(f'Te quedan {intentos_login} intentos restantes.')
                 self._user = input('----------------------------------------------\nIngresá tu nombre de usuario o correo: ')
+                self._password = input('----------------------------------------------\nIngresá la contraseña: ')
+            else: print('\n---------------------------------\nAgotaste todos los intentos..\n       Acceso denegado...\n')
 
+
+    def menu_admin(self):#se presenta el menu de opciones de administrador
+        print('------------------------------------------\nBienvenid@ al sistema de gestión de turnos\n--Ingresaste como usuario Administrador--\n------------------------------------------')
+        print('Por favor digite la opcion a realizar:')
+        print('Operacion 1: Listar Turnos')        
+        print('Operacion 2: Eliminar Turnos')
+        print('Operacion 3: Consultar Clientes')
+        Operacion = int(input('Digite el numero de la opcion: '))
+        while Operacion < 1 or Operacion > 3:
+            if Operacion == 1:
+                admin.validar_user
+            elif Operacion == 2:
+                admin.validar_user
+            elif Operacion == 3:
+                admin.validar_user
+            else: print('Opcion incorrecta, por favor intente de nuevo')
 
             
 admin = Admin('', '') #se instancia un objeto vacío de la clase para validar
