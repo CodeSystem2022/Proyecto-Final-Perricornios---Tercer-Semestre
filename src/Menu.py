44de44769728fe747af9c1d803d24c8fa770cd97
ChuequitoMendez <112666288+ChuequitoMendez@users.noreply.github.com>
2023-06-20 18:53:01 -0300

    Update Menu.py

--- src/Menu.py
+++ src/Menu.py
@@ -69,6 +69,23 @@ class Menu:
             else:
                 print("Opción inválida") 
 
+    def menu_admin(self):  # se presenta el menu de opciones de administrador
+        print('------------------------------------------\nBienvenid@ al sistema de gestión de turnos\n--Ingresaste como usuario Administrador--\n------------------------------------------')
+        print('Por favor digite la opcion a realizar:')
+        print('Operacion 1: Listar Turnos')        
+        print('Operacion 2: Eliminar Turnos')
+        print('Operacion 3: Consultar Clientes')
+        Operacion = int(input('Digite el numero de la opcion: '))
+        while Operacion < 1 or Operacion > 3:
+            Operacion = int(input('Opcion incorrecta, por favor intente de nuevo: '))
+        if Operacion == 1:
+            Conexion.listarTurnos()
+        elif Operacion == 2:
+            Conexion.eliminarTurnos()
+        elif Operacion == 3:
+            Conexion.consultarClientes()
+
+
 
 menu = Menu()
 menu.mostrar_menu()
